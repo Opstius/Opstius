034ed9a0255b9d450bdd096f8c4a280128e7d480
Puobit <puobit@gmail.com>
2025-02-27 11:32:17 +0530

    DMGenerator created successfully.

---
+++ Opstius.Generator.MVVM/DMGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Opstius.Generator.MVVM.Models;
+using System.Text;
+
+namespace Opstius.Generator.MVVM
+{
+    public static class DMGenerator
+    {
+        public static string Generate(DMClass model)
+        {
+            return GenerateClass(model);
+        }
+        private static string GenerateClass(DMClass model)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"[XmlRoot(\"{model.Name}\")]");
+            builder.AppendLine($"public sealed class {model.DataModelName}");
+            builder.AppendLine($"{{");
+
+            foreach (DMProperty property in model.Properties)
+            {
+                builder.AppendLine(GenerateProperty(property));
+            }
+
+            builder.AppendLine($"}}");
+
+            return builder.ToString();
+        }
+        private static string GenerateProperty(DMProperty model)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            string value = Utilities.GetDMValue(model.Type, model.Value);
+
+            builder.AppendLine($"[XmlElement(\"{model.Name}\")]");
+            builder.AppendLine($"[JsonPropertyName(\"{model.Name}\")]");
+            builder.AppendLine($"public {model.Type} {model.Name} {{ set; get; }} = {value};");
+
+            return builder.ToString();
+        }
+    }
+}
