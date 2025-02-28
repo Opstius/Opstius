293625cd96966cea8348d5c8a869707082e5b123
Puobit <puobit@gmail.com>
2025-02-28 21:30:13 +0530

    RandomNumberGenerator added.

---
+++ Opstius.Generator.RandomData/RandomNumberGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace Opstius.Generator.RandomData
+{
+    public static class RandomNumberGenerator
+    {
+        public static string Generate(int length)
+        {
+            return RandomStringGenerator.Generate("0123456789", length);
+        }
+    }
+}
